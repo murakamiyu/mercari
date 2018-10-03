e9cc9f74525ae886e84742d4359329d356f5d3b3
森田賢 <moritaken@moritakennoMacBook-puro.local>
2018-10-03 11:54:24 +0900

    mypage_settei

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -4,4 +4,31 @@ def index
 	render action: 'mypage-sidebar'
 end
 
+def profile
+end
+
+def deliver_adress
+end
+
+def card
+end
+
+def card_new
+end
+
+def card_after
+end
+
+def email_pass
+end
+
+def identification
+end
+
+def sms_confirmation
+end
+
+def logout
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root 'tests#index'
   get 'mypages/index', to: 'mypages#index'
+  get 'mypages/profile', to: 'mypages#profile'
+  get 'mypages/deliver_adress', to: 'mypages#deliver_adress'
+  get 'mypages/card', to: 'mypages#card'
+  get 'mypages/card_new', to: 'mypages#creditnew'
+  get 'mypages/card_after', to: 'mypages#creditafter'
+  get 'mypages/email_password', to: 'mypages#email_password'
+  get 'mypages/identification', to: 'mypages#identification'
+  get 'mypages/sms_confirmation', to: 'mypages#sms_confirmation'
+  get 'mypages/logout', to: 'mypages#logout'
 end
