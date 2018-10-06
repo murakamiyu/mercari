f370f38f317a7ec427fefc662d6d5c8bfe43ed71
森田賢 <moritaken@moritakennoMacBook-puro.local>
2018-10-06 17:57:57 +0900

    微修正

---
+++ app/assets/javascripts/newaccount.js
@@ -0,0 +1,61 @@
+
+// $(function() {
+//   $(".pc-header-nav-ro").on('click', function(e){
+//     e.preventDefault();
+//     console.log(this)
+//     $("#wrapper1").css('display', 'block');
+//   });
+// });
+
+
+
+$(function() {
+  $(".btn-mail, .btn-sns-google, .btn-sns-facebook").on('click', function(e){
+    e.preventDefault();
+    console.log(this)
+    $("#wrapper1,#wrapper3,#wrapper4,#wrapper5,#wrapper6").css('display', 'none');
+    $("#wrapper2").css('display', 'block');
+  });
+});
+
+
+$(function() {
+  $(".btn-info").on('click', function(e){
+    e.preventDefault();
+    console.log(this)
+    $("#wrapper1,#wrapper2,#wrapper4,#wrapper5,#wrapper6").css('display', 'none');
+    $("#wrapper3").css('display', 'block');
+  });
+});
+
+
+
+
+$(function() {
+  $(".btn-sms").on('click', function(e){
+    e.preventDefault();
+    console.log(this)
+    $("#wrapper3").css('display', 'none');
+    $("#wrapper4").css('display', 'block');
+  });
+});
+
+$(function() {
+
+  $(".btn-adress").on('click', function(e){
+    e.preventDefault();
+    console.log(this);
+    $("#wrapper4").css('display', 'none');
+    $('#wrapper5').css('display', 'block');
+  });
+});
+
+$(function() {
+
+  $(".btn-credit").on('click', function(e){
+    e.preventDefault();
+    console.log(this);
+    $("#wrapper5").css('display', 'none');
+    $('#wrapper6').css('display', 'block');
+  });
+});
